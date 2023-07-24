6eb79896f18b97c7300d892009d52c1a6502b56d
lydialebdiri <lydialebdiri2021@gmail.com>
2023-07-24 18:36:12 +0200

    final react app for visualisation

---
+++ pred-sales-visualization/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "finalvisualizationd3bc051cd3bc051c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
